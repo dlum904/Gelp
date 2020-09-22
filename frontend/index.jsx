ee94fc360a1598ab933596cea7daaef020cfea9f
dennis <dlum904@gmail.com>
2020-09-22 17:57:02 -0400

    added app.jsx and root.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-
+    const store = configureStore();
+    window.getState = store.getState;
+    // window.dispatch = store.dispatch;
     // window.signup = signup;
     // window.login = login;
     // window.logout = logout;
 
-    ReactDOM.render(<h1>Welcome to Yelp!</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
+
+// user = { 1: { id: 1, first_name: "dennis", last_name: "lum", email: "dlum@.com", zip_code: "11234" } }
