be2ff01f7f1c0de1bd2d72911eb6f5af11c881ca
dennis <dlum904@gmail.com>
2021-02-03 15:54:35 -0500

    deleted some comments

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -32,16 +32,6 @@ const App = () => (
             <Route path="/" component={Splash} />
         </Switch>
 
-        {/* <footer>
-            <div className="mylinks">
-                <p className="about-user-title">About</p>
-                <a href="https://www.linkedin.com/in/dennis-lum-12526b7a/" target="_blank">Linkedin</a>
-                <a href="https://github.com/dlum904" target="_blank"> Git Hub</a>
-                <a href="https://angel.co/u/dennis-lum" target="_blank"> Angel List</a>
-                <a href="https://dlum904.github.io/dlum/" target="_blank"> About Me</a>
-            </div>
-            <img src={window.footerURL} alt=""/>
-        </footer> */}
     </div>
 );
 
