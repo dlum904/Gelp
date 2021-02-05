81c0cff8504d8e783cb927ed260c34684579551c
dennis <dlum904@gmail.com>
2021-02-05 13:50:33 -0500

    update app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -28,7 +28,6 @@ const App = () => (
             <ProtectedRoute exact path="/user" component={UserShowContainer} />
             <Route path="/" component={Splash} />
         </Switch>
-
     </div>
 );
 
